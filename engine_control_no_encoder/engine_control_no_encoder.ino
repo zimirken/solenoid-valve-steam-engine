0ccb7736556512f18c606cdec5a26f026c6b8625
zimirken <zimlink@gmail.com>
2017-12-22 11:43:50 -0500

    Add files via upload

---
+++ engine_control_no_encoder/engine_control_no_encoder.ino
@@ -0,0 +1,229 @@
+/* This is the control program for a steam engine that uses a 5 port 3 position closed center solenoid valve to drive an industrial air cylinder.
+ *  My previous engine designs used a rotary encoder, but this one will use a magnet attached to the piston rod that travels past a set of 4 hall sensors. 
+ *  Two at TDC and BDC, and two that are in the middle that can be physically moved to control cutoff. This should make things waaaay simpler.
+ */
+
+//using the button parts of the automation library to simplify the cylinder position sensing
+#include <Automaton.h>    
+
+
+#include "Arduino.h"
+
+#include <digitalWriteFast.h>  // library for high performance reads and writes by jrraines
+                               // see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
+                               // and http://code.google.com/p/digitalwritefast/
+ 
+
+
+//disable debugging once everything works
+//#define EnableDebug
+
+#ifdef EnableDebug
+unsigned long previousMillis = 0;  
+const long interval = 1000;
+#endif
+
+
+static int TDCPin = 2;
+static int BDCPin = 3;
+static int TopCutoffPin = 4;
+static int BottomCutoffPin = 5;
+static int EngineEnablePin = 7;
+static int Solenoid1Pin = 10;     //solenoid 1 controls top solenoid
+static int Solenoid2Pin = 11;     //solenoid 2 controls bottom solenoid
+static int AdvancePin = 6;
+
+
+
+
+int CycleState = 1;
+/*  
+ *  1 = top cylinder admission
+ *  2 = top cylinder expansion
+ *  3 = bottom cylinder admission
+ *  4 = bottom cylinder expansion
+ */
+
+int Advance = 0;
+int EEnable = 0;
+
+
+
+Atm_button TDC, BDC, TopCutoff, BottomCutoff;
+
+void setup()
+{
+  #ifdef EnableDebug
+  Serial.begin(115200);
+  #endif
+ 
+ 
+  
+  pinMode(EngineEnablePin, INPUT_PULLUP);
+  pinMode(AdvancePin, INPUT_PULLUP);
+
+  TDC.begin(TDCPin)
+    .debounce(0)
+    .onPress(TDC_Press)
+    .onRelease(TDC_Release);
+
+  BDC.begin(BDCPin)
+    .debounce(0)
+    .onPress(BDC_Press)
+    .onRelease(BDC_Release);
+
+  TopCutoff.begin(TopCutoffPin)
+    .debounce(0)
+    .onPress(TopCutoff_Press)
+    .onRelease(TopCutoff_Release);
+
+  BottomCutoff.begin(BottomCutoffPin)
+    .debounce(0)
+    .onPress(BottomCutoff_Press)
+    .onRelease(BottomCutoff_Release);
+
+  pinMode(Solenoid1Pin, OUTPUT);    
+  pinMode(Solenoid2Pin, OUTPUT);    
+
+  
+
+}
+ 
+void loop()
+{
+  //this loop includes no delays or anything, should execute as fast as reasonable
+
+
+   //read switch states
+   Advance = digitalReadFast(AdvancePin);
+   EEnable = digitalReadFast(EngineEnablePin);
+
+
+   //state program
+   automaton.run();
+   
+   
+   //all the valve control is handled in this sub
+   SolenoidControl();
+
+
+   
+   #ifdef EnableDebug
+
+   unsigned long currentMillis = millis();
+
+   if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    Serial.print("Advance Pin:");
+    Serial.println(Advance);
+    Serial.print("State:");
+    Serial.println(CycleState);
+   }
+   #endif
+   
+   
+   
+}
+
+//when advance is enabled, the cylinder states change on rising. When advance is disabled, cylinder states change on falling.
+void TDC_Press(int idx, int v, int up) {
+  if (Advance == 0) {
+    CycleState = 1;
+  }
+}
+
+void TopCutoff_Press(int idx, int v, int up) {
+  if (Advance == 0) {
+    if (CycleState == 1) {
+      CycleState = 2;
+    }
+  }
+}
+
+void BDC_Press(int idx, int v, int up) {
+  if (Advance == 0) {
+    CycleState = 3;
+  }
+}
+
+void BottomCutoff_Press(int idx, int v, int up) {
+  if (Advance == 0) {
+    if (CycleState == 3) {
+      CycleState = 4;
+    }
+  }
+}
+
+//advance disabled
+void TDC_Release(int idx, int v, int up) {
+  if (Advance == 1) {
+    CycleState = 1;
+  }
+}
+
+void TopCutoff_Release(int idx, int v, int up) {
+  if (Advance == 1) {
+    if (CycleState == 1) {
+      CycleState = 2;
+    }
+  }
+}
+
+void BDC_Release(int idx, int v, int up) {
+  if (Advance == 1) {
+    CycleState = 3;
+  }
+}
+
+void BottomCutoff_Release(int idx, int v, int up) {
+  if (Advance == 1) {
+    if (CycleState == 3) {
+      CycleState = 4;
+    }
+  }
+}
+
+
+
+//here is the important part.
+ 
+void SolenoidControl() {
+
+  //disable the engine if the engine enable switch is off
+  if (EEnable == 1) {
+    digitalWriteFast(Solenoid1Pin, LOW);
+    digitalWriteFast(Solenoid2Pin, LOW);
+    return;
+  }
+
+
+  //top admission
+  if (CycleState == 1) {
+    digitalWriteFast(Solenoid1Pin, HIGH);
+    digitalWriteFast(Solenoid2Pin, LOW);
+  }
+  //top expansion
+  if (CycleState == 2) {
+    digitalWriteFast(Solenoid1Pin, LOW);
+    digitalWriteFast(Solenoid2Pin, LOW);
+  }
+  //bottom admission
+  if (CycleState == 3) {
+    digitalWriteFast(Solenoid1Pin, LOW);
+    digitalWriteFast(Solenoid2Pin, HIGH);
+  }
+  //bottom expansion
+  if (CycleState == 4) {
+    digitalWriteFast(Solenoid1Pin, LOW);
+    digitalWriteFast(Solenoid2Pin, LOW);
+  }  
+  
+}
+  
+  
+  
+  
+  
+  
+  
---
+++ steam2propaneservo/steam2propaneservo.ino
@@ -0,0 +1,231 @@
+#include <Servo.h>
+#include <LowPower.h>
+
+/*
+steam scooter control program
+
+
+
+
+*/
+
+
+
+//Port assignments
+//analog inputs
+//const int batteryvoltagepin = A1;
+const int pressuresensorpin = A0;
+const int waterlevelanalog = A3;
+
+//digital inputs
+
+const int throttle = 11;
+const int power = 9;
+
+
+
+//digital outputs
+const int valveservo = 3;
+const int waterpump = 7;
+const int pressuredisplaypin = 6;         //neopixel string for pressure display and battery display
+//const int batterydisplay = 9;          //single neopixel for battery display
+const int waterpumpdisplay = 13;         //led lights up when water is detected
+
+//timers and other constants
+const int waterpumpinterval = 15;      //interval to check the water level at
+const int shutdowntimer = 20;         //timer to slowly shutdown the pellet burner to avoid excessive smoke or overheating
+const int glowplugtimer = 90;         //glowplug firing duration on startup or relight
+const int pressurehigh = 100;          //maximum pressure, fan to idle, pressure may go higher due to idling fan and relief valve
+const int pressurelow = 70;            //low pressure setting for fan control
+const int PIXEL_COUNT = 8;              //neopixel count
+const int batterypixel = 7;            //battery pixel
+const float batteryhigh = 13.0;        //high voltage battery level
+const float batterylow = 11.0;         //low voltage battery level
+const float batterydivider = 0.25;      //ratio for battery voltage divider.
+const long startuptime = 10;         //timer for propane ignition.
+const int waterlevelset = 400;        //set point for analog water level sensor
+const int pressureoff = 120;          //pressure for turning off propane burner.
+
+const int propaneoff = 10;            //servo position for propane off
+const int propaneprelight = 30;      //servo position for propane prelight, gas on but before ignition.
+const int propanehigh = 50;           //servo position for high burn.
+const int propanelow = 100;         //servo position for low burn.
+
+
+long waterprevious = 0;
+long shutdownprevious = 0;
+int powerbuttonState = 0;         // current state of the button
+int powerlastButtonState = 0;     // previous state of the button
+int pressure = 0;
+int temperature1 = 0;            
+float temperature2 = 0;
+long batterylevel = 0;
+unsigned long currentMillis = 0;
+int startupcheck = 0;
+long startuptimer = 0;
+int waterlevela = 0;
+
+Servo propane;
+
+
+
+
+
+void setup() {
+  //congifure ports
+  pinMode(throttle, INPUT);
+  digitalWrite(throttle, HIGH);
+  pinMode(power, INPUT);
+  digitalWrite(power, HIGH);
+  pinMode(waterpump, OUTPUT);
+  pinMode(waterpumpdisplay, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(waterpump,LOW);
+
+  propane.attach(valveservo);
+  
+
+}
+
+void loop() {
+  
+  currentMillis = millis(); //update current time
+//  temperature1 = 100*analogRead(thermocouple1pin); //update temperature
+  if (analogRead(waterlevelanalog) > waterlevelset ) {
+    waterlevela = 1;
+  }
+  if (analogRead(waterlevelanalog) < waterlevelset) {
+    waterlevela = 0;
+  }
+  digitalWrite(waterpumpdisplay,waterlevela);
+  //battery voltage
+  //batterylevel = map((analogRead(batteryvoltagepin)/batterydivider),0,1024,0,5);
+  
+  //read pressure sensor and convert
+  pressure = map(analogRead(pressuresensorpin),102,922,0,200); //maps the analog read to the pressure, this is based on the specs of the pressure sensor  
+  
+  Serial.print("Pressure:");
+  Serial.println(pressure);
+  Serial.print("Temperature1:");
+  Serial.println(temperature1);
+  Serial.print("Water Level:");
+  Serial.println(analogRead(waterlevelanalog));
+  Serial.print("Power Switch:");
+  Serial.println(digitalRead(power));
+  
+  
+  //shutdown section
+  powerbuttonState = digitalRead(power);
+    // compare the buttonState to its previous state
+    if (powerbuttonState != powerlastButtonState) {
+      shutdownprevious = currentMillis;
+      startuptimer = currentMillis;
+      powerlastButtonState = powerbuttonState;
+    }      
+  if (0==digitalRead(power)) {    
+    shutdownsequence();
+    startupcheck = 1;
+  }
+   
+  if (currentMillis - startuptimer > (startuptime*1000)) {
+    startupcheck = 0;
+  }
+  //check and maintain water level every interval
+  if(currentMillis - waterprevious > (waterpumpinterval*1000)) {
+    waterprevious = currentMillis;
+    waterpumpfunc();
+  }
+   
+  //this function controls the neopixel strip
+//  pressuredisplay();
+  
+  //pressure based fire control function
+  if (1==digitalRead(power)) {  
+    pressurecontrol();
+  }
+  
+  delay(200);  
+  
+  
+}
+
+
+
+
+
+
+void shutdownsequence() {
+  if(currentMillis - shutdownprevious > (shutdowntimer*1000)) {
+    propane.write(propaneoff);
+   // digitalWrite(waterpump, 0);
+    
+    
+    while(0 == digitalRead(power)){
+      LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+    }
+  }
+  else{
+   propane.write(propaneoff);
+    
+  }
+}
+  
+  
+//function turns on water pump if water sensor is low until water sensor is high         I should add code later to prevent boiler flooding    
+void waterpumpfunc(){
+ if (analogRead(waterlevelanalog) > waterlevelset ) {
+   digitalWrite(waterpump, HIGH);
+   //digitalWrite(waterpumpdisplay, HIGH);
+   while (analogRead(waterlevelanalog) > waterlevelset ) {
+       Serial.print("Pressure:");
+  Serial.println(pressure);
+  Serial.print("Temperature1:");
+  Serial.println(temperature1);
+  Serial.print("Water Level:");
+  Serial.println(analogRead(waterlevelanalog) );
+  Serial.print("Power Switch:");
+  Serial.println(digitalRead(power));
+     delay(10);
+   }
+   digitalWrite(waterpump, LOW);
+   //digitalWrite(waterpumpdisplay, LOW);
+ }
+}
+  
+  
+
+
+
+//the function that controls the fan and glow plug based on pressure and temperature
+void pressurecontrol() {      
+  //ignition state
+  if (startupcheck == 1 && pressure < pressurelow) {
+    propane.write(propaneprelight);
+    return;
+  }
+  if (pressure > pressureoff) {
+    propane.write(propaneoff);
+    startupcheck = 1;
+    return;
+  }
+  //low pressure state
+  if (pressure < pressurelow) {
+    propane.write(propanehigh);
+    return;
+  }
+  //high pressure state
+  if (pressure > pressurehigh) {
+    propane.write(propanelow);
+    return;
+  }
+  //medium pressure state, the important one
+  if (pressure < pressurehigh && pressure > pressurelow) {
+    propane.write(map(pressure, pressurelow, pressurehigh, propanehigh, propanelow));
+    return;
+  }
+}
+
+
+
+
+
